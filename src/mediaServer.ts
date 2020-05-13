b3ffc42bf66384379d34b45925103e5d623e9ce7
atanumallick <atanu.mallick@questionpro.com>
2020-05-13 19:52:17 +0530

    test

--- src/mediaServer.ts
+++ src/mediaServer.ts
@@ -22,24 +22,24 @@ export class MediaServer {
                 mediaroot: './media',
                 allow_origin: '*'
             },
-            trans: {
-                ffmpeg: '/usr/bin/ffmpeg',
-                tasks: [
-                    {
-                        app: 'live',
-                        vc: "copy",
-                        vcParam: [],
-                        ac: "aac",
-                        acParam: ['-ab', '64k', '-ac', '1', '-ar', '16000'],
-                        rtmp:true,
-                        rtmpApp:'live2',
-                        hls: true,
-                        hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
-                        dash: true,
-                        dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
-                    }
-                ]
-            }
+            // trans: {
+            //     ffmpeg: '/usr/bin/ffmpeg',
+            //     tasks: [
+            //         {
+            //             app: 'live',
+            //             vc: "copy",
+            //             vcParam: [],
+            //             ac: "aac",
+            //             acParam: ['-ab', '64k', '-ac', '1', '-ar', '16000'],
+            //             rtmp:true,
+            //             rtmpApp:'live2',
+            //             hls: true,
+            //             hlsFlags: '[hls_time=2:hls_list_size=3:hls_flags=delete_segments]',
+            //             dash: true,
+            //             dashFlags: '[f=dash:window_size=3:extra_window_size=5]'
+            //         }
+            //     ]
+            // }
         });
     }
 
