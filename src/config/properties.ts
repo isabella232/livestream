b9ef284072f93105d20fa4b1ee49df76d767891d
atanumallick <atanu.mallick@questionpro.com>
2020-05-13 18:32:00 +0530

    config

--- src/config/properties.ts
+++ src/config/properties.ts
@@ -4,5 +4,5 @@ dotenv.config();
 export let properties = {
     sessionKey: process.env.SESSION_KEY,
     jwtToken: process.env.JWT_SECRET,
-    revAIToken: process.env.REV_AI_TOKEN
+    revAIToken: '02JAzLskvm-ikK7ROlzEs_i1W57PNvvE8p5kuBJfqU9TAIjaHAl8_bFFbAG2Xr78wwxevqva9yvpY8BMuP4lky7Fmt2pg'
 };
